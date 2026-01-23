88d2574d99fae135fea6eda29014dac640d16ac8
Kristoff Aadryk L. Viray <ezeaydab@gmail.com>
2026-01-23 06:13:30 +0000

    added elements for app.vue

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,11 +1,10 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>Frutas</h1>
+  <personal-profile/>
+  <food-item/>
+  <food-item2/>
 </template>
 
-<style scoped></style>
+<script></script>
+
+<style></style>
