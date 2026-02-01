f63567ff54c4346ad7df6045de41514177a39c7c
Kristoff Aadryk L. Viray <ezeaydab@gmail.com>
2026-02-01 17:01:37 +0000

    task 03 create a clean project

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <h1>Frutas</h1>
-  <personal-profile/>
+  <h1>Food</h1>
   <food-item/>
-  <food-item2/>
+  <food-item/>
+  <food-item/>
 </template>
-
+ 
 <script></script>
 
-<style></style>
+<style>
+ #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+</style>
