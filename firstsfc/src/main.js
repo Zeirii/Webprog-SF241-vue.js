87356ecaa32dc47cdc925a94bf57b571f39713dd
Kristoff Aadryk L. Viray <ezeaydab@gmail.com>
2026-02-10 05:51:57 +0800

    Task 03 add view and insert comments/visitor feedback functionality to your website

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -5,11 +5,16 @@ import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/Fooditem2.vue'
 import PersonalProfile from './components/Personalprofile.vue'
 import './components/css/personalprofile.css'
+import CommentForm from './components/CommentForm.vue'
+import Comment from './components/Comment.vue'
+
 
 const app = createApp(App)
 
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
 app.component('personal-profile', PersonalProfile)
+app.component('comment-form', CommentForm)
+app.component('comment', Comment)
 
 app.mount('#app')
