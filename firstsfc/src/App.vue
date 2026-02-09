05942d8c45362951bfa0dd3959b2bda211642323
Kristoff Aadryk L. Viray <ezeaydab@gmail.com>
2026-02-09 20:45:54 +0800

    Task 04 wk 09

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,16 +1,21 @@
-<template>
-  <h1>{{ message }}</h1>
-  <personal-profile />
-  <food-item />
-  <food-item2 />
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      message: 'Welcome to my Portfolio'
-    };
-  }
-};
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
+ 
+const instruments = ref([])
+ 
+async function getInstruments() {
+  const { data } = await supabase.from('instruments').select()
+  instruments.value = data
+}
+ 
+onMounted(() => {
+   getInstruments()
+})
 </script>
+ 
+<template>
+  <ul>
+    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+  </ul>
+</template>
